2246a8cbcca5f65ba1dec03047fe7d0c1fce6ad2
Syeda Anika Jerin <anikajerin2@gmail.com>
2026-01-27 20:26:56 +0600

    [ADD:]Uploader option

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     package_data={
         "geomap3d": ["templates/*.html"],
     },
-    install_requires=[], # We use standard libs mostly!
-    description="A pure Python library for interactive 3D maps.",
-    author="Your Name",
+    install_requires=[], 
+    description="A pure Python library for interactive 3D mapping.",
+    author="Syeda Anika Jerin",
 )
